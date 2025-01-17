e74ab867db2d29a93ea4d53ada8535a33fa4998d
Bukunmi Akinyemi <bkakinyemi21@gmail.com>
2025-01-17 22:59:53 +0100

    WEWERE

--- app/middleware.py
+++ app/middleware.py
@@ -31,7 +31,7 @@ def register_middleware(app: FastAPI):
         allow_credentials = True,
     )
 
-    app.add_middleware(
-        TrustedHostMiddleware,
-        allowed_hosts = ["localhost", "127.0.0.1"]
-    )
+    # app.add_middleware(
+    #     TrustedHostMiddleware,
+    #     allowed_hosts = ["localhost", "127.0.0.1"]
+    # )
